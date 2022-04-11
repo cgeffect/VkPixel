117bd199e648a527a15ee0002ff936d64a3454ea
Jason <JasonBee@163.com>
2022-04-11 12:50:49 +0800

    VkPixel Init

---
+++ VkPixel/VkPixel/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  VkPixel
+//
+//  Created by Jason on 2022/4/11.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ VkPixel/VkPixel/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  VkPixel
+//
+//  Created by Jason on 2022/4/11.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
